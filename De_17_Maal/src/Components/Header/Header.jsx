42b22a0a94f3c542dce6ad4005499b2e823f800d
Highog92 <Madsserritslev@gmail.com>
2023-08-15 09:52:26 +0200

    Header Færdig

---
+++ De_17_Maal/src/Components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import style from './Header.module.scss'
+import HeaderColorWheel from '../../assets/images/ColorWheel.png'
+
+
+export function Header() {
+
+    return (
+        <header>
+            <img src={HeaderColorWheel} alt="Top Color Wheel" />
+            <hgroup>
+                <h1>
+                    The Global Goals
+                </h1>
+                <h2>For sustainable Developement</h2>
+            </hgroup>
+
+        </header>
+
+
+
+    )
+
+
+}
